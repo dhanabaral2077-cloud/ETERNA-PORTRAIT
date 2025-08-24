8c61450d826ab7d0229883e0d5bf43f658f3283c
Data Is Beautiful <dhanabaral2077@gmail.com>
2025-08-24 14:34:01 +0000

    1. Hero Section (First Impression)
    2. Showcase / Gallery Section
    
    3. H

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,10 +21,10 @@ export default function Home() {
         <AnimatedSection>
           <HowItWorks />
         </AnimatedSection>
-        <Story />
         <AnimatedSection>
           <Testimonials />
         </AnimatedSection>
+        <Story />
         <AnimatedSection>
           <Pricing />
         </AnimatedSection>
