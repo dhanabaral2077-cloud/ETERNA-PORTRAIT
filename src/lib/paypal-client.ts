e697aa9f270b938c7da58c9e6c0b69aab3702407
Data Is Beautiful <dhanabaral2077@gmail.com>
2025-08-26 06:56:58 +0000

    commit

--- src/lib/paypal-client.ts
+++ src/lib/paypal-client.ts
@@ -5,7 +5,7 @@ const configureEnvironment = () => {
   const clientId = process.env.PAYPAL_CLIENT_ID;
   const clientSecret = process.env.PAYPAL_CLIENT_SECRET;
 
-  if (!clientId || !clientSecret || clientId === 'YOUR_PAYPAL_CLIENT_ID' || clientSecret === 'YOUR_PAYPAL_CLIENT_SECRET') {
+  if (!clientId || !clientSecret || clientId === 'AfKhNNhb1354EtlPPaOXNYM4q4e9WRS3neGrhBRfsrK73pTYVA3TqfxG9UB9q7axpiWK30j-qejQWTlx' || clientSecret === 'EAkcAjM_D_L0x6tcb37nrMH4m2qp7TqqNb4-5FLr-NoAgtS61BHTD5opTiUrUj6aQXxPG7wHZYHDOqqs') {
     throw new Error("PayPal client ID or secret is missing or a placeholder. Check environment variables.");
   }
   
